74adcd69872eea31fd18ef1a5255546ffe5a95d3
Jonathan Poon <jonathanysp@gmail.com>
2014-07-12 00:14:45 -0700

    mongo

--- db/index.js
+++ db/index.js
@@ -13,8 +13,7 @@ exports.reset = function *() {
 	yield db.get('Venues').drop();
 	yield db.get('Stalls').drop();
 	yield db.get('Msgs').drop();
-	db.get('Counter');
-	yield db.get('Counters').drop();
+	yield db.get('Counters')
 
 	var Counters = db.get('Counters');
 
