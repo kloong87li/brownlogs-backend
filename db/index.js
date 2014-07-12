429b4ab2e58d77746bd19c6800004ff8aea4f2e8
Jonathan Poon <jonathanysp@gmail.com>
2014-07-12 00:05:41 -0700

    add reset

--- db/index.js
+++ db/index.js
@@ -8,3 +8,27 @@ var Msgs = require('./Msgs.js')(db, Counters);
 exports.Venues = Venues;
 exports.Stalls = Stalls;
 exports.Msgs = Msgs;
+
+exports.reset = function *() {
+	yield db.get('Venues').drop();
+	yield db.get('Stalls').drop();
+	yield db.get('Msgs').drop();
+	yield db.get('Counters').drop();
+
+	var Counters = db.get('Counters');
+
+	yield Counters.insert({
+		collection: 'Venues',
+		seq: 0.
+	});
+
+	yield Counters.insert({
+		collection: 'Stalls',
+		seq: 0.
+	});
+
+	yield Counters.insert({
+		collection: 'Msgs',
+		seq: 0.
+	});
+}
